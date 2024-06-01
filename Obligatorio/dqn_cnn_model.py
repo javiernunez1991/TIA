5d4dc92f954a99c3db022966be807b4739bfe54a
javiernunez1991 <74318366+javiernunez1991@users.noreply.github.com>
2024-06-01 07:44:48 -0300

    Add files via upload

---
+++ Obligatorio/dqn_cnn_model.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN_CNN_Model(nn.Module):
+    def __init__(self,  env_inputs, n_actions):
+        # Implementar.
+        pass
+
+    def forward(self, env_input):
+        # Implementar. 
+        pass
