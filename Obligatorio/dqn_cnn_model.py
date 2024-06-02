f181ab42063c53502af78030176a140377c478e5
javiernunez1991 <74318366+javiernunez1991@users.noreply.github.com>
2024-06-02 16:45:58 -0300

    Update dqn_cnn_model.py

--- Obligatorio/dqn_cnn_model.py
+++ Obligatorio/dqn_cnn_model.py
@@ -4,33 +4,35 @@ import torch.nn as nn
 # import torch.nn.functional as F
 
 class DQN_CNN_Model(nn.Module):
-    def __init__(self,  env_inputs, n_actions):
+    def __init__(self, env_inputs, n_actions):
         super().__init__()
         
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=env_inputs[0], out_channels=16, kernel_size=8, stride=4), # env_inputs[0] = 4 frames
-            nn.ReLU(),
+            nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4), # env_inputs[0] = 4 frames
+            nn.ReLU(), # aplico reLU sobre los kernels
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2),
-            nn.ReLU()          
+            nn.ReLU() # aplico reLU sobre los kernels         
             )
         
-        conv_out_size = self._get_conv_out(env_inputs) # 4x84x84 = 28224 neuronas ocultas
+        salida_conv_size = self._get_conv_out(env_inputs) # ver explicacion en _get_conv_out
        
         self.fc = nn.Sequential(
-            nn.Linear(conv_out_size, 256), # las 28224 fully conected con 256 neuronas
-            nn.ReLU(),
-            nn.Linear(256, n_actions)
+            nn.Linear(salida_conv_size, 256), # conecto las neuronas obtenidas con 256 nuevas neuronas ocultas
+            nn.ReLU(), # aplico reLU
+            nn.Linear(256, n_actions) # conecto las 256 neuronas ocultas, con la capa final de 6 neuronas (tantas como acciones posibles)
         )        
         
     def _get_conv_out(self, shape):
-        #o = self.conv(torch.zeros(1, *shape))
-        o = torch.zeros(1, *shape)
-        return int(np.prod(o.size()))        
+        o = self.conv(torch.zeros(1, *shape)) 
+          # Creo un tensor de 0s, con el mismo shape que el parametro env_inputs
+          # Le aplico las mismas capas de convolución que le aplicaria a las imagenes
+        o = int( np.prod(o.size()) ) # obtengo la cantidad final de neuronas que necesito
+        return o 
 
 
     def forward(self, x):
-        conv_out = self.conv(x).view(x.size()[0], -1)
-        return self.fc(conv_out)        
+        conv_out = self.conv(x).view(x.size()[0], -1) # Aplano los feature maps
+        return self.fc(conv_out)  
         
 
 
