6dfa93d657d38c3f21c0d09c8b146169ed322e66
javiernunez1991 <74318366+javiernunez1991@users.noreply.github.com>
2024-06-03 21:32:32 -0300

    Update replay_memory.py

--- Obligatorio/replay_memory.py
+++ Obligatorio/replay_memory.py
@@ -1,30 +1,24 @@
 import random
 from collections import namedtuple, deque
-import numpy as np
-import torch
+# import numpy as np
+# import torch
 
 Transition = namedtuple('Experience', ['state', 'action', 'reward', 'done', 'next_state'])
 
 class ReplayMemory:
 
-    def __init__(self, buffer_size, device):
+    def __init__(self, buffer_size):
         self.buffer_size = buffer_size # numero maximo de experiences que almacena la replay memory        
         self.memory = deque(maxlen=buffer_size) # objeto similar a lista pero de largo fijo
-        self.experience = namedtuple('Experience', ['state', 'action', 'reward', 'done', 'next_state'])
-        self.device = device
+        self.experience = Transition('state', 'action', 'reward', 'done', 'next_state')
     
     def add(self, state, action, reward, done, next_state):
         e = self.experience(state, action, reward, done, next_state) 
         self.memory.append(e) # Agrega una nueva experiencia a la memoria
 
     def sample(self, batch_size):
-        batch = random.sample(self.memory, k=self.batch_size)
-        states = torch.from_numpy(np.vstack([e.state for e in batch if e is not None])).float().to(self.device)
-        actions = torch.from_numpy(np.vstack([e.action for e in batch if e is not None])).long().to(self.device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in batch if e is not None])).float().to(self.device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in batch if e is not None])).float().to(self.device)
-        dones = torch.from_numpy(np.vstack([e.done for e in batch if e is not None]).astype(np.uint8)).float().to(self.device)
-        return (states, actions, rewards, next_states, dones)
+        batch = random.sample(self.memory, k=self.batch_size) # obtiene una muestra aleatoria de tamaño batch_size
+        return batch
 
     def __len__(self):
         return len(self.memory) # Tamaño actual de la memoria
