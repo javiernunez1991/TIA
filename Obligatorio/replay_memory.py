506b6d9d12a76cbf7abc2aed2b4a4a5969746736
javiernunez1991 <74318366+javiernunez1991@users.noreply.github.com>
2024-06-04 21:34:26 -0300

    Update replay_memory.py

--- Obligatorio/replay_memory.py
+++ Obligatorio/replay_memory.py
@@ -1,6 +1,6 @@
 import random
 from collections import namedtuple, deque
-import numpy as np
+# import numpy as np
 # import numpy as np
 # import torch
 
@@ -25,24 +25,22 @@ class ReplayMemory:
 
 ## CHEQUEO SI FUNCIONA  
 # buffer_size = 7
-# batch_size = 3  
+# batch_size = 3
 # replay = ReplayMemory(buffer_size)
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 11, 111, True, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 22, 222, False, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 33, 333, True, process_state(np.random.rand(4, 84, 84))) 
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 44, 444, True, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 55, 555, False, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 66, 666, True, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 77, 777, False, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 88, 888, False, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 99, 999, True, process_state(np.random.rand(4, 84, 84)))
-# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32)), 1010, 101010, False, process_state(np.random.rand(4, 84, 84)))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 0, 111, True, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 5, 222, False, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 4, 333, True, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE)) 
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 2, 444, True, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 1, 555, False, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 0, 666, True, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 3, 777, False, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 3, 888, False, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 4, 999, True, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
+# replay.add(process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE), 2, 101010, False, process_state(np.random.rand(4, 84, 84).astype(np.float32), DEVICE))
 # mini_batch = replay.sample(batch_size)
 # states = torch.stack([mini_batch[i].state for i in range(batch_size)])
-# print(f'states.size: {states.size()}')
 # actions = torch.tensor([mini_batch[i].action for i in range(batch_size)])
 # rewards = torch.tensor([mini_batch[i].reward for i in range(batch_size)])
 # dones = torch.tensor([int(mini_batch[i].done) for i in range(batch_size)])
-# next_state = torch.stack([mini_batch[i].next_state for i in range(batch_size)])
+# next_states = torch.stack([mini_batch[i].next_state for i in range(batch_size)])
 
-# net(states)
