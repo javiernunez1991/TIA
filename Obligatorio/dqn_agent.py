2a8e88185733b3c65d1635e549015c34a61cc429
javiernunez1991 <74318366+javiernunez1991@users.noreply.github.com>
2024-06-15 14:40:00 -0300

    Update dqn_agent.py

--- Obligatorio/dqn_agent.py
+++ Obligatorio/dqn_agent.py
@@ -1,13 +1,8 @@
 import torch
-#import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-# from torch.utils.tensorboard import SummaryWriter
-# from replay_memory import ReplayMemory, Transition
 from abstract_agent import Agent
-# from tqdm.notebook import tqdm
 import numpy as np
-
 
 
 class DQNAgent(Agent):
@@ -51,30 +46,28 @@ class DQNAgent(Agent):
 
             # Obtener un minibatch de la memoria. Resultando en tensores de estados, acciones, recompensas, flags de terminacion y siguentes estados. 
             mini_batch = self.memory.sample(self.batch_size)
-            states = torch.stack([mini_batch[i].state for i in range(self.batch_size)])#.to(self.device)
+            states = torch.stack([mini_batch[i].state for i in range(self.batch_size)])
             actions = torch.tensor([mini_batch[i].action for i in range(self.batch_size)]).to(self.device) 
             rewards = torch.tensor([mini_batch[i].reward for i in range(self.batch_size)]).to(self.device)
             dones = torch.tensor([int(mini_batch[i].done) for i in range(self.batch_size)]).to(self.device) # paso los T-F, a 1-0
-            next_states = torch.stack([mini_batch[i].next_state for i in range(self.batch_size)])#.to(self.device)
+            next_states = torch.stack([mini_batch[i].next_state for i in range(self.batch_size)])
 
             # Obetener el valor estado-accion (Q) de acuerdo a la policy net para todo elemento (estados) del minibatch.
             q_values = self.policy_net(states) # states es un minibatch de: (batch_size x 4 x 84 x 84)
-            state_q_values = q_values.gather(1, actions.unsqueeze(1))#.squeeze()#.cpu().sum().item() #ACTIONS VECTOR VERTICAL 
+            state_q_values = q_values.gather(1, actions.unsqueeze(1))
             
 
             # Obtener max a' Q para los siguientes estados (del minibatch). Es importante hacer .detach() al resultado de este computo.
             # Si el estado siguiente es terminal (done) este valor debería ser 0.
             next_q_values = self.policy_net(next_states).detach()
-            q_next_states = torch.max(next_q_values, dim=1)#.cpu().sum().item()
+            q_next_states = torch.max(next_q_values, dim=1)
             max_next_q_values = q_next_states.values.detach()
         
 
             # Compute el target de DQN de acuerdo a la Ecuacion (3) del paper.    
             target = rewards + (1 - dones) * self.gamma * max_next_q_values
 
-
-            # Compute el costo y actualice los pesos.
-            # En Pytorch la funcion de costo se llaman con (predicciones, objetivos) en ese orden.
+            # Compute el costo y actualice los pesos. En Pytorch la funcion de costo se llaman con (predicciones, objetivos) en ese orden.
             loss = F.mse_loss(target.unsqueeze(1), state_q_values) # Calculate el error
             loss.backward()  # Backpropagate loss
             self.optimizer.step()  # Update model weights
