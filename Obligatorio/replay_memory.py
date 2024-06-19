f7f312373d1ebb2881f41d82b283841f0b928f61
javiernunez1991 <74318366+javiernunez1991@users.noreply.github.com>
2024-06-19 19:58:44 -0400

    Update replay_memory.py

--- Obligatorio/replay_memory.py
+++ Obligatorio/replay_memory.py
@@ -1,8 +1,5 @@
 import random
 from collections import namedtuple, deque
-# import numpy as np
-# import numpy as np
-# import torch
 
 Transition = namedtuple('Experience', ['state', 'action', 'reward', 'done', 'next_state'])
 
@@ -21,28 +18,3 @@ class ReplayMemory:
 
     def __len__(self):
         return len(self.memory) # Tamaño actual de la memoria
-
-
-## CHEQUEO SI FUNCIONA  
-# def random_exper(dev):
-#   return process_state(np.random.rand(4, 84, 84).astype(np.float32), dev)
-
-# buffer_size = 7
-# batch_size = 3
-# replay = ReplayMemory(buffer_size)
-# replay.add(random_exper(DEVICE), 0, 111, True, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 5, 222, False, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 4, 333, True, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 2, 444, True, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 1, 555, False, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 0, 666, True, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 3, 777, False, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 3, 888, False, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 4, 999, True, random_exper(DEVICE))
-# replay.add(random_exper(DEVICE), 2, 101010, False, random_exper(DEVICE))# mini_batch = replay.sample(batch_size)
-# states = torch.stack([mini_batch[i].state for i in range(batch_size)])
-# actions = torch.tensor([mini_batch[i].action for i in range(batch_size)])
-# rewards = torch.tensor([mini_batch[i].reward for i in range(batch_size)])
-# dones = torch.tensor([int(mini_batch[i].done) for i in range(batch_size)])
-# next_states = torch.stack([mini_batch[i].next_state for i in range(batch_size)])
-
